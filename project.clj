ae15b9ac386361a9a05ed58489a8b9f812f0a12f
Bor Hodošček <bor.hodoscek@gmail.com>
2013-08-19 20:41:08 +0900

    start next development iteration

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject clj-mecab "0.1.0"
+(defproject clj-mecab "0.2.0-SNAPSHOT"
   :description "Clojure wrapper for the Japanese Morphological Analyzer MeCab"
   :url "https://github.com/borh/clj-mecab"
+  :licence {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :licenses [{:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
              {:name "BSD"
               :url "BSD"}]
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.chasen/mecab "0.996"]]
+  :dependencies [[org.chasen/mecab "0.996"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :main clj-mecab.parse)
