18d17ef2a90f4a63293593c62e5f8443daa4fb2e
Bor Hodošček <bor.hodoscek@gmail.com>
2016-04-21 01:43:11 +0900

    updated deps and version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-mecab "0.4.6"
+(defproject clj-mecab "0.4.7"
   :description "Clojure wrapper for the Japanese Morphological Analyzer MeCab"
   :url "https://github.com/borh/clj-mecab"
   :licence {:name "Eclipse Public License"
@@ -9,11 +9,11 @@
               :url "BSD"}]
   :scm {:url "https://github.com/borh/clj-mecab.git"
         :name "git"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.nativelibs4java/bridj "0.7.0"]
                  [net.moraleboost.cmecab-java/cmecab-java "2.1.0"]
                  [org.clojure/data.csv "0.1.3"]
-                 [prismatic/schema "1.0.3"]]
+                 [prismatic/schema "1.1.0"]]
   :jvm-opts ["-server"]
-  :profiles {:dev {:dependencies [[criterium "0.4.3"]]}}
+  :profiles {:dev {:dependencies [[criterium "0.4.4"]]}}
   :main ^:skip-aot clj-mecab.parse)
